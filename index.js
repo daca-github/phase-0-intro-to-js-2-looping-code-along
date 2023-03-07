a95b7b831ac2f541195effe28fcbe5b5574c1a14
Daniel Cornejo <danielcornejoaaa@gmail.com>
2023-03-07 18:05:03 -0500

    Complete

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function writeCards(names, eventName) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+      const message = `Thank you, ${names[i]}, for the wonderful ${eventName} gift!`;
+      messages.push(message);
+    }
+    return messages;
+  }
+
+function countDown(){
+    let countDownn = 10;
+    while (countDownn >= 0){
+        console.log(countDownn--);
+    }
+    return countDownn
+}
+
